ca7a5cf2b302361987c283368f99085664b6934e
luyang <yang.lu@nanguadl.com>
2018-01-08 10:06:05 +0800

    add

---
+++ src/main/java/com/demo/file/RowTest.java
@@ -0,0 +1,35 @@
+package com.demo.file;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by DL-NG-ZJ-0019 on 2018/1/5.
+ */
+public class RowTest {
+    public static void main(String[] agrs){
+        for (int j = 0; j < 100; j++) {
+            System.out.println("11");
+        }
+
+        getMap();
+
+    }
+
+    private static void getMap() {
+        Map<String,Object> ad= new HashMap<String,Object>();
+        if (ad != null) {
+
+
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
